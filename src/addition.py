be8149cbb1d4f7245a9b14a1c531c1ecfd9e7cfd
prempeddamallu <prem.peddamallu@datagrokr.com>
2024-08-22 17:14:41 +0530

    First Commit

---
+++ src/addition.py
@@ -0,0 +1,34 @@
+# app.py
+# This is a test commit
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero.")
+    return x / y
+
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_divide():
+    assert divide(6, 3) == 2
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(1, 0)
+
